2102b247281e4b5b57a5235f0a0786ed4026f482
Charlie Chen <charliechen@exosite.com>
2018-10-24 11:32:35 +0800

    Install jsondecode terraform provider

--- build_atlas_provider.sh
+++ build_atlas_provider.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
+
 go get github.com/akshaykarle/terraform-provider-mongodbatlas
-cd src/github.com/akshaykarle/terraform-provider-mongodbatlas/
+pushd "$GOPATH/src/github.com/akshaykarle/terraform-provider-mongodbatlas/"
 git remote add exosite https://github.com/exosite/terraform-provider-mongodbatlas.git
 git fetch exosite
 git checkout import_cluster
 make build
+popd
+
+go get github.com/EvilSuperstars/terraform-provider-jsondecode
+pushd "$GOPATH/src/github.com/EvilSuperstars/terraform-provider-jsondecode"
+make build
+popd
